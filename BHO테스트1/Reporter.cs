d2f81012f9e77f8e6b6dabdaf2b36a172fa17362
byeonggonlee@gmail.com <byeonggonlee@gmail.com@bdc10bb4-361b-e515-3df7-60fa7826a2aa>
2013-05-30 16:41:22 +0000

    신고 UI 작업중 + 이것저것

---
+++ BHO테스트1/Reporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BHO테스트1
+{
+    public partial class Reporter : Form
+    {
+        public Reporter()
+        {
+            InitializeComponent();
+        }
+    }
+}
