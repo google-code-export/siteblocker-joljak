8343b174c14669978d165d94bea9cfe7a7eb6709
byeonggonlee@gmail.com <byeonggonlee@gmail.com@bdc10bb4-361b-e515-3df7-60fa7826a2aa>
2013-06-15 11:06:48 +0000

    이름 변경 및 GSB(Google Safe Browsing) 연동 검사 추가

---
+++ Site Blocker/Site Blocker/IObjectWithSite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SHDocVw;
+using mshtml;
+using System.IO;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace Site_Blocker
+{
+    [ComVisible(true),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown),
+    Guid("140DA4BA-52F9-41D1-9AEB-6B8B4F52DC4D")]
+    interface IObjectWithSite
+    {
+        [PreserveSig]
+        int SetSite([MarshalAs(UnmanagedType.IUnknown)]object site);
+        [PreserveSig]
+        int GetSite(ref Guid guid, out IntPtr ppvSite);
+    }
+}
