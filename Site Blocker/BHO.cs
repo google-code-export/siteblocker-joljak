ed7ae13b1794759f5d6c538c23bc9b9bda4cae62
byeonggonlee@gmail.com <byeonggonlee@gmail.com@bdc10bb4-361b-e515-3df7-60fa7826a2aa>
2013-06-16 09:23:42 +0000

    아래 커밋할 때 실수해서 다시 커밋

---
+++ Site Blocker/BHO.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SHDocVw;
+using mshtml;
+using System.IO;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.IO.Pipes;
+
+namespace Site_Blocker
+{
+    [
+            ComVisible(true),
+            Guid("2159CB25-EF9A-54C1-B43C-E30D1A4A8277"),
+            ClassInterface(ClassInterfaceType.None)
+    ]
+    public class BHO : IObjectWithSite
+    {
+        private WebBrowser webBrowser;
+        private String BeforeURL = "";
+        public int SetSite(object site)
+        {
+            if (site != null)
+            {
+                webBrowser = (WebBrowser)site;
+                /*webBrowser.DocumentComplete +=
+                  new DWebBrowserEvents2_DocumentCompleteEventHandler(
+                  this.OnDocumentComplete);*/
+                webBrowser.NavigateComplete2 += new DWebBrowserEvents2_NavigateComplete2EventHandler(this.webBrowser_NavigateComplete2);
+            }
+            else
+            {
+                /*webBrowser.DocumentComplete -=
+                  new DWebBrowserEvents2_DocumentCompleteEventHandler(
+                  this.OnDocumentComplete);*/
+                webBrowser.NavigateComplete2 += new DWebBrowserEvents2_NavigateComplete2EventHandler(this.webBrowser_NavigateComplete2);
+                webBrowser = null;
+            }
+
+            return 0;
+
+        }
+
+        public int GetSite(ref Guid guid, out IntPtr ppvSite)
+        {
+            IntPtr punk = Marshal.GetIUnknownForObject(webBrowser);
+            int hr = Marshal.QueryInterface(punk, ref guid, out ppvSite);
+            Marshal.Release(punk);
+            return hr;
+        }
+
+
+
+        public void OnDocumentComplete(object pDisp, ref object URL)
+        {
+            HTMLDocument document = (HTMLDocument)webBrowser.Document;
+
+
+            //HTMLDocument document = (HTMLDocument)webBrowser.Document;
+
+            /*IHTMLElement head = (IHTMLElement)((IHTMLElementCollection)
+                                   document.all.tags("head")).item(null, 0);
+            IHTMLScriptElement scriptObject =
+              (IHTMLScriptElement)document.createElement("script");
+            scriptObject.type = @"text/javascript";
+            scriptObject.text = "alert('aaa');";
+            ((HTMLHeadElement)head).appendChild((IHTMLDOMNode)scriptObject);*/
+
+            /*
+            // If the site or url is null, do not continue
+            if (pDisp == null || URL == null) return;
+            // Grab the document object off of the WebBrowser control
+           
+            if (document == null) return;
+            // Report the total number of links on the current page
+            MessageBox.Show("Total links on this page: " +
+            document.links.length.ToString());
+             */
+        }
+
+        void webBrowser_NavigateComplete2(object pDisp, ref object URL)
+        {
+            // If the site or url is null, do not continue
+            if (pDisp == null || URL == null) return;
+            
+            // Access both the web browser object and the url passed
+            // to this event handler
+            SHDocVw.WebBrowser browser = (SHDocVw.WebBrowser)pDisp;
+            string url = URL.ToString();
+
+            if (!BeforeURL.Equals(url))
+            {
+                BeforeURL = url;
+                // Grab the document object off of the Web Browser control
+                IHTMLDocument2 document = (IHTMLDocument2)webBrowser.Document;
+                if (document == null) return;
+
+                int rating = DBConnector.GetSiteInfo(url);
+
+                if (rating <= 0)
+                {
+                    // This is Safe Site.
+                    // Pass the current URL to the broker
+                    PassUrlToBroker(url);
+                }
+                else if (rating >= 1 && rating <= 25)
+                {
+                    // This is Reported Site. But Not Blocked Site
+                    System.Windows.Forms.MessageBox.Show("Reported");
+                    // Pass the current URL to the broker
+                    PassUrlToBroker(url);
+                }
+                else if (rating >= 26 && rating <= 75)
+                {
+                    // This is Reported Site.
+                    System.Windows.Forms.MessageBox.Show("Danger. But Accessable");
+                    // Pass the current URL to the broker
+                    PassUrlToBroker(url);
+                }
+                else if (rating >= 76 && rating <= 100)
+                {
+                    // This is Reported Site.
+                    System.Windows.Forms.MessageBox.Show("Danger. Blocked");
+                    browser.Stop();
+                    document.clear();
+                    document.close();
+                    browser.Navigate2("about:blank", true);
+                }
+            }
+        }
+
+        public void PassUrlToBroker(string url)
+        {
+            // Create a new named pipe client object
+            NamedPipeClientStream pipeClient = null;
+
+            try
+            {
+
+                // Grab a new instance of a named pipe cleint
+                // stream, connecting to the same named pipe as the
+                // server- BhoPipeName
+                pipeClient = new NamedPipeClientStream(
+                    ".",
+                    "BhoPipeExample",
+                    PipeDirection.InOut,
+                    PipeOptions.None
+                    );
+
+                // Attempt a connection with a 2 second limit
+                pipeClient.Connect(0);
+
+                //
+                pipeClient.ReadMode = PipeTransmissionMode.Message;
+
+                // Once connected, pass the url to the server
+                pipeClient.Write(Encoding.Unicode.GetBytes(url), 0,
+                    Encoding.Unicode.GetBytes(url).Length);
+
+                // Wait for the message to complete
+                while (!pipeClient.IsMessageComplete) ;
+
+            }
+            catch (Exception) { }
+            finally
+            {
+                // Close the pipe client once complete (if exists)
+                if (pipeClient != null)
+                {
+                    pipeClient.Close();
+                    pipeClient = null;
+                }
+            }
+        }
+
+
+
+        public const string BHO_REGISTRY_KEY_NAME =
+   "Software\\Microsoft\\Windows\\" +
+   "CurrentVersion\\Explorer\\Browser Helper Objects";
+
+        [ComRegisterFunction]
+        public static void RegisterBHO(Type type)
+        {
+            RegistryKey registryKey =
+              Registry.LocalMachine.OpenSubKey(BHO_REGISTRY_KEY_NAME, true);
+
+            if (registryKey == null)
+                registryKey = Registry.LocalMachine.CreateSubKey(
+                                        BHO_REGISTRY_KEY_NAME);
+
+            string guid = type.GUID.ToString("B");
+            RegistryKey ourKey = registryKey.OpenSubKey(guid);
+
+            if (ourKey == null)
+            {
+                ourKey = registryKey.CreateSubKey(guid);
+            }
+
+            ourKey.SetValue("NoExplorer", 1, RegistryValueKind.DWord);
+
+            registryKey.Close();
+            ourKey.Close();
+        }
+
+        [ComUnregisterFunction]
+        public static void UnregisterBHO(Type type)
+        {
+            RegistryKey registryKey =
+              Registry.LocalMachine.OpenSubKey(BHO_REGISTRY_KEY_NAME, true);
+            string guid = type.GUID.ToString("B");
+
+            if (registryKey != null)
+                registryKey.DeleteSubKey(guid, false);
+        }
+    }
+}
---
+++ Site Blocker/Reporter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Site_Blocker
+{
+    
+    public partial class Reporter : Form
+    {
+        bool ischecked = false;
+        static bool firstcheck = true;
+        public Reporter()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!ischecked && firstcheck)
+            {
+                DialogResult dResult = MessageBox.Show("이 항목을 통해 신고를 하시면 SiteBlocker에 이메일 주소를 제공하게 됩니다. 그래도 전송하시겠습니까", "경고", MessageBoxButtons.YesNo);
+                switch (dResult)
+                {
+                    case DialogResult.Yes:
+                        firstcheck = false;
+                        ischecked = true;
+                        this.checkBox1.Checked = true;
+                        button1.Enabled = true;
+                        break;
+                    case DialogResult.No:
+                        ischecked = false;
+                        this.checkBox1.Checked = false;
+                        button1.Enabled = false;
+                        break;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String URL = textBox1.Text;
+            String Reporter = textBox2.Text;
+            bool check = checkBox1.Checked;
+            int status = 0;
+
+            if (URL.Length <= 0)
+                status = status + 1;
+            if (Reporter.Length <= 0)
+                status = status + 2;
+            switch (status)
+            {
+                case 1:
+                    MessageBox.Show("URL이 입력되지 않았습니다.");
+                    break;
+                case 2:
+                    MessageBox.Show("신고자의 이메일 주소가 입력되지 않았습니다.");
+                    break;
+                case 3:
+                    MessageBox.Show("URL 및 이메일 주소가 입력되지 않았습니다.");
+                    break;
+                default:
+                    DBConnector.ReportSite(URL, Reporter);
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
